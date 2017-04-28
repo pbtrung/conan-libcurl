df151298ae14e6f98022e3259c2baa94f60b0602
Trung Pham <phambaotrung@gmail.com>
2017-04-28 07:44:44 -0700

    Enable libssh2

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(args="--build missing")
     builder.add_common_builds(pure_c=True, shared_option_name="libcurl:shared")
     builder.run()
